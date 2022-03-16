60be237bd13a0b9bd6828d43f76641c229dfea51
JD Solanki <jdsolanki0001@gmail.com>
2022-03-16 10:36:14 +0530

    init

---
+++ src/select.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+interface Props {
+  label?: string
+}
+
+interface SelectProps extends Props {}
+
+const props = withDefaults(defineProps<SelectProps>(), {
+  label: 'Email',
+})
+</script>
+
+<template>
+  {{ props.label }}
+</template>
